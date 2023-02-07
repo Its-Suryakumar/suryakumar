b4edd57dcfb38fc837239a9920f4068b3c7f4469
surya-stark007 <suryakumar36531@gmail.com>
2023-02-07 11:11:49 +0530

    Updates

---
+++ precache-manifest.6fce594b95d8e1c507615db703e0b0e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7375b751c88032b5121",
+    "url": "/suryakumar/static/css/main.f5b5aa05.chunk.css"
+  },
+  {
+    "revision": "a7375b751c88032b5121",
+    "url": "/suryakumar/static/js/main.a7375b75.chunk.js"
+  },
+  {
+    "revision": "f7a2aa984837e5567f15",
+    "url": "/suryakumar/static/js/1.f7a2aa98.chunk.js"
+  },
+  {
+    "revision": "601008d4c12651bc83a7",
+    "url": "/suryakumar/static/js/runtime~main.601008d4.js"
+  },
+  {
+    "revision": "6d6a6886ac163a15b7f4b7eb094d85b9",
+    "url": "/suryakumar/static/media/Resume.6d6a6886.pdf"
+  },
+  {
+    "revision": "4bc646b01efa756ad50428b9d3364c9f",
+    "url": "/suryakumar/static/media/me.4bc646b0.png"
+  },
+  {
+    "revision": "d59d737d00f7fcab2997fca8e6afad7b",
+    "url": "/suryakumar/static/media/me-about.d59d737d.jpeg"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/suryakumar/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "e361062d1c833282b12edb6eb53126bb",
+    "url": "/suryakumar/index.html"
+  }
+];
