993b5a78b4e0115605c0c8de2da4f2f0aa8328c8
surya-stark007 <suryakumar36531@gmail.com>
2023-06-21 18:40:53 +0530

    Experience bug:

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,7 +12,7 @@ const footer = () => {
       <ul className='permalinks'>
         <li><a href='#'>Home</a></li>
         <li><a href='#about'>About</a></li>
-        <li><a href='#eperience'>Experience</a></li>
+        <li><a href='#experience'>Experience</a></li>
         <li><a href='#portfolio'>Portfolio</a></li>
         <li><a href='#testimonials'>Testimonials</a></li>
         <li><a href='#contact'>Contact</a></li>
