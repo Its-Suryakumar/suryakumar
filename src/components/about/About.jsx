65251c81f8e4c4e3921f27b0a2f436de4e17dd3e
surya-stark007 <suryakumar36531@gmail.com>
2023-12-16 13:37:55 +0530

    Experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
               <article className='about__card'>
                   <FaAward className='about__icon'/>
                   <h5>Experience</h5>
-                  <small>Ex-Intern @ Aerobiosys Innovations<br/>Open to Work</small>
+                  <small>Ex-Intern @ Aerobiosys Innovations</small>
               </article>
               <article className='about__card'>
                   <BiCodeAlt className='about__icon'/>
